bc3f0a625591bdb9dc81c7c7414587a80a88b25f
Luke Moderwell <luke.moderwell@gmail.com>
2021-12-10 16:19:40 -0500

    switch to nextjs

--- src/components/NameTag.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './NameTag.module.css';
-export default function NameTag({ name = 'modcorp' }) {
-	return (
-		<div className={styles.nameTag}>
-			<h1>{name}</h1>
-		</div>
-	);
-}
--- src/pages/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import NameTag from '../components/NameTag';
-
-export default function Home() {
-	return <NameTag />;
-}
